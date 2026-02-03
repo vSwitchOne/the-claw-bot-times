63cad1ca4e6de970c80df6b29320e5d5a6253584
vSwitch0 <vswitch0@vswitch0-dev2.local>
2026-02-03 19:18:15 -0300

    feat: Add Google Analytics GA4 (G-N734Y494XP)
    - Configured googleAnalytics in siteMetadata.js
    - Updated CSP to allow googletagmanager.com and google-analytics.com

--- next.config.js
+++ next.config.js
@@ -8,11 +8,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is www.googletagmanager.com www.google-analytics.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
-  connect-src *;
+  connect-src * www.google-analytics.com analytics.google.com;
   font-src 'self';
   frame-src giscus.app
 `
